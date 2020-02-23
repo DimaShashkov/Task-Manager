94e9edfab094b766ff136be44bbc4079387592d3
dimas <shashkovdima981@gmail.com>
2020-02-23 17:43:44 +0300

    Initialization

---
+++ src/test/java/gradwork/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package gradwork.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
